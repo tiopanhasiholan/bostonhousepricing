1e2235805b3b136e496ab29ff3debe1824ad748c
tiopanhasiholan <84217042+tiopanhasiholan@users.noreply.github.com>
2024-04-09 14:12:02 +0700

    web app is ready

--- app.py
+++ app.py
@@ -24,6 +24,18 @@ def predict_api():
     print(output[0])
     return jsonify({'prediction': output[0]})
 
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    final_input = scaler.transform(np.array(data).reshape(1, -1))
+    print(final_input)
+
+    # print(np.array(list(data.values())).reshape(1, -1))
+    # new_data = scaler.transform(np.array(list(data.values())).reshape(1, -1))
+
+    output = reg_model.predict(final_input)[0]
+    return render_template('home.html', prediction_text='Predicted Price is {}'.format(output))
+
 if __name__ == '__main__':
     app.run(debug=True)
     
